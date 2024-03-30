21195b8a6b356b333418ef7bee1a4ef19274cacb
Alison Rampton <alison.rampton@gmail.com>
2024-03-30 17:00:57 -0500

    changed alignment and background boxes for category buttons

--- app/class_card.tsx
+++ app/class_card.tsx
@@ -5,7 +5,7 @@ export default function ClassCard({ classObj }: { classObj: Class }) {
     <div
       className="w-full mt-10 h-auto bg-sky-900 
       hover:bg-sky-800 focus:bg-sky-700 focus:ring focus:ring-yellow-400 rounded-md shadow-md hover:shadow-lg 
-      transition-all duration-1000 ease-in-out flex-col group"
+      transition-all duration-1000 ease-in-out flex-col group cursor-pointer"
       tabIndex={-2}
     >
       <h1 className="text-left text-white text-xl font-semibold font-sans pl-4 pt-2 pr-2">
@@ -15,11 +15,11 @@ export default function ClassCard({ classObj }: { classObj: Class }) {
         {classObj.credits.creditHours.value} credit hours
       </h2>
       {classObj.description ? (
-        <p className="transition-all delay-0 duration-0 group-hover:delay-75 group-hover:duration-200 group-focus:delay-75 group-focus:duration-200 ease-in-out h-0 opacity-0 group-hover:opacity-100 group-hover:h-auto group-focus:opacity-100 group-focus:h-auto text-left text-white font-sans pl-4 p-2">
+        <p className="cursor-auto transition-all delay-0 duration-0 group-hover:delay-75 group-hover:duration-200 group-focus:delay-75 group-focus:duration-200 ease-in-out h-0 opacity-0 group-hover:opacity-100 group-hover:h-auto group-focus:opacity-100 group-focus:h-auto text-left text-white font-sans pl-4 p-2">
           {classObj.description}
         </p>
       ) : (
-        <p className="transition-all delay-0 duration-0 group-hover:delay-75 group-hover:duration-200 group-focus:delay-75 group-focus:duration-200 ease-in-out h-0 opacity-0 group-hover:opacity-100 group-hover:h-auto group-focus:opacity-100 group-focus:h-auto text-left text-slate-300 font-sans pl-4 p-2">
+        <p className="cursor-auto transition-all delay-0 duration-0 group-hover:delay-75 group-hover:duration-200 group-focus:delay-75 group-focus:duration-200 ease-in-out h-0 opacity-0 group-hover:opacity-100 group-hover:h-auto group-focus:opacity-100 group-focus:h-auto text-left text-slate-300 font-sans pl-4 p-2">
           No description found.
         </p>
       )}
